568a14f0d5ea08681658aa63417250bc30fd4a56
theo.fratia <theofratia22@gmail.com>
2025-05-20 11:07:01 +0200

    add secret

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3000
+;
 
 app.get('/status', (req, res) => {
   res.json({ status: 'OK', timestamp: new Date() });
